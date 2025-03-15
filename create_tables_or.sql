896ac3fc0edcd2cfc4a2b4a0170fe10fd08eec64
Alice Oliveira <aliceoliveira@velocityglobal.com>
2025-03-15 20:49:00 -0300

    create methods

--- create_tables_or.sql
+++ create_tables_or.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE TYPE tp_entregador AS OBJECT (
     Nome VARCHAR(100)
     );
 /
+
 ALTER TYPE tp_entregador ADD ATTRIBUTE Idade NUMBER CASCADE;
 /
 -- desfazendo mudança
@@ -17,6 +18,7 @@ CREATE TABLE tb_entregador OF tp_entregador (
     Nome NOT NULL
 );
 /
+
 CREATE OR REPLACE TYPE tp_consumidor AS OBJECT (
     CPF CHAR(11),
     DataDeNascimento DATE,
@@ -28,12 +30,14 @@ CREATE OR REPLACE TYPE tp_consumidor AS OBJECT (
     Complemento VARCHAR(50)
 );
 /
+
 CREATE TABLE tb_consumidor OF tp_consumidor (
     CPF PRIMARY KEY,
     DataDeNascimento NOT NULL,
     Nome NOT NULL
 );
 /
+
 CREATE OR REPLACE TYPE tp_fornecedor AS OBJECT (
     CNPJ CHAR(14),
     Nome VARCHAR(100),
@@ -44,41 +48,50 @@ CREATE OR REPLACE TYPE tp_fornecedor AS OBJECT (
     Complemento VARCHAR(50)
 ) NOT FINAL ;
 /
+
 CREATE TABLE tb_fornecedor OF tp_fornecedor (
     CNPJ PRIMARY KEY,
     Nome NOT NULL
 );
 /
+
 CREATE OR REPLACE TYPE tp_supermercado UNDER tp_fornecedor();
 /
+
 CREATE TABLE tb_supermercado OF tp_supermercado (
     CNPJ PRIMARY KEY
 );
 /
+
 CREATE OR REPLACE TYPE tp_restaurante UNDER tp_fornecedor (
     Categoria VARCHAR(50)
 );
 /
+
 CREATE TABLE tb_restaurante OF tp_restaurante (
     CNPJ PRIMARY KEY
 );
 /
+
 CREATE OR REPLACE TYPE tp_produto AS OBJECT (
     IdProduto INT,
     Nome VARCHAR(100)
 );
 /
+
 CREATE TABLE tb_produto OF tp_produto (
     IdProduto PRIMARY KEY,
     Nome NOT NULL
 );
 /
+
 CREATE OR REPLACE TYPE tp_produtoOfertado AS OBJECT (
     IdProduto REF tp_produto,
     CNPJ_Forn REF tp_fornecedor,
     Preco DECIMAL(10, 2)
 );
 /
+
 CREATE TABLE tb_produtoOfertado OF tp_produtoOfertado(
    --  CONSTRAINT PK_ProdutoOfertado PRIMARY KEY (IdProduto, CNPJ_Forn),
     idProduto WITH ROWID REFERENCES tb_produto,
@@ -86,6 +99,7 @@ CREATE TABLE tb_produtoOfertado OF tp_produtoOfertado(
     Preco CHECK (Preco >= 0)
 );
 /
+
 CREATE OR REPLACE TYPE tp_pedido AS OBJECT (
     IdPedido INT,
     DataEntrega DATE,
@@ -95,6 +109,7 @@ CREATE OR REPLACE TYPE tp_pedido AS OBJECT (
     CNPJ_Fornecedor REF tp_fornecedor
 );
 /
+
 CREATE TABLE tb_pedido OF tp_pedido (
     IdPedido PRIMARY KEY,
     DataEntrega NOT NULL,
@@ -103,12 +118,14 @@ CREATE TABLE tb_pedido OF tp_pedido (
     CNPJ_Fornecedor WITH ROWID REFERENCES tb_fornecedor
 );
 /
+
 CREATE OR REPLACE TYPE tp_contem AS OBJECT (
     IdPedido REF tp_pedido,
     IdProduto REF tp_produto,
     IdFornecedor REF tp_fornecedor
 );
 /
+
 CREATE TABLE tb_contem OF tp_contem(
    -- CONSTRAINT PK_Contem PRIMARY KEY (IdPedido, IdProduto, IdFornecedor),
     IdPedido WITH ROWID REFERENCES tb_pedido NOT NULL,
@@ -123,49 +140,58 @@ CREATE OR REPLACE TYPE tp_telefoneEntregador AS OBJECT (
     CPF REF tp_entregador
 );
 /
+
 CREATE TABLE tb_telefoneEntregador OF tp_telefoneEntregador (
     CONSTRAINT PK_TelefoneEntregador PRIMARY KEY (Numero, Ddd),
     CPF SCOPE IS tb_entregador
 );
 /
+
 CREATE OR REPLACE TYPE tp_telefoneConsumidor AS OBJECT (
     Ddd CHAR(2),
     Numero CHAR(9),
     CPF REF tp_consumidor
 );
 /
+
 CREATE TABLE tb_telefoneConsumidor OF tp_telefoneConsumidor (
     CONSTRAINT PK_TelefoneConsumidor PRIMARY KEY (Numero, Ddd),
     CPF SCOPE IS tb_consumidor
 );
 /
+
 CREATE OR REPLACE TYPE tp_telefoneFornecedor AS OBJECT (
     Ddd CHAR(2),
     Numero CHAR(9),
     CNPJ REF tp_fornecedor
 );
 /
+
 CREATE TABLE tb_telefoneFornecedor OF tp_telefoneFornecedor (
     CONSTRAINT PK_TelefoneFornecedor PRIMARY KEY (Numero, Ddd),
     CNPJ SCOPE IS tb_fornecedor
 );
 /
+
 CREATE OR REPLACE TYPE tp_desconto AS OBJECT (
     CNPJ REF tp_fornecedor,
     Data DATE,
     Porcentagem DECIMAL(5, 2)
 );
 /
+
 CREATE TABLE tb_desconto OF tp_desconto (
     CONSTRAINT PK_Desconto PRIMARY KEY (Data),
     CNPJ WITH ROWID REFERENCES tb_fornecedor
 );
 /
+
 CREATE OR REPLACE TYPE tp_componente AS OBJECT (
     CodigoComponente REF tp_produto,
     CodigoComposto REF tp_produto
 );
 /
+
 CREATE TABLE tb_componente OF tp_componente (
   --  CONSTRAINT PK_Componente PRIMARY KEY (CodigoComponente, CodigoComposto),
     CodigoComponente WITH ROWID REFERENCES tb_produto NOT NULL,
@@ -173,3 +199,89 @@ CREATE TABLE tb_componente OF tp_componente (
 );
 /
 -- DUVIDA: REF NÃO ENTRA NA PK?
+
+CREATE OR REPLACE TYPE BODY tp_entregador AS
+    MEMBER FUNCTION getIdade RETURN NUMBER IS
+    BEGIN
+       RETURN TRUNC(MONTHS_BETWEEN(SYSDATE, SELF.DataDeNascimento)/12);
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE BODY tp_produto AS
+    CONSTRUCTOR FUNCTION tp_produto(IdProduto INT, Nome VARCHAR) RETURN SELF AS RESULT IS
+    BEGIN
+         SELF.IdProduto := IdProduto;
+         SELF.Nome := Nome;
+         RETURN;
+    END;
+    
+    ORDER MEMBER FUNCTION compare(p tp_produto) RETURN INTEGER IS
+    BEGIN
+         RETURN SELF.IdProduto - p.IdProduto;
+    END;
+    
+    MAP MEMBER FUNCTION map_function RETURN INT IS
+    BEGIN
+         RETURN SELF.IdProduto;
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE tp_nested_produtoOfertado AS TABLE OF tp_produtoOfertado;
+/
+
+ALTER TYPE tp_pedido ADD ATTRIBUTE (Itens tp_nested_produtoOfertado) CASCADE;
+/
+
+CREATE OR REPLACE TYPE BODY tp_pedido AS
+    MEMBER PROCEDURE addItem(p tp_produtoOfertado) IS
+    BEGIN
+         IF SELF.Itens IS NULL THEN
+            SELF.Itens := tp_nested_produtoOfertado();
+         END IF;
+         SELF.Itens.EXTEND;
+         SELF.Itens(SELF.Itens.LAST) := p;
+    END;
+   
+    MEMBER FUNCTION calcularTotal RETURN DECIMAL IS
+         total DECIMAL(10,2) := 0;
+    BEGIN
+         IF SELF.Itens IS NOT NULL THEN
+             FOR i IN 1..SELF.Itens.COUNT LOOP
+                 total := total + SELF.Itens(i).Preco;
+             END LOOP;
+         END IF;
+         RETURN total;
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE BODY tp_fornecedor AS
+    MEMBER FUNCTION getIdentificador RETURN VARCHAR2 FINAL IS
+    BEGIN
+         RETURN 'Fornecedor: ' || SELF.CNPJ;
+    END;
+    
+    MEMBER FUNCTION getTipo RETURN VARCHAR2 IS
+    BEGIN
+         RETURN 'Fornecedor';
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE BODY tp_restaurante AS
+    OVERRIDING MEMBER FUNCTION getTipo RETURN VARCHAR2 IS
+    BEGIN
+         RETURN 'Restaurante - ' || SELF.Categoria;
+    END;
+END;
+/
+
+CREATE OR REPLACE TYPE BODY tp_supermercado AS
+    OVERRIDING MEMBER FUNCTION getTipo RETURN VARCHAR2 IS
+    BEGIN
+         RETURN 'Supermercado';
+    END;
+END;
+/
--- populate_or.sql
+++ populate_or.sql
@@ -1,1 +1,86 @@
--- fazer povoamento baseado no povoamento inicial
+-- INSERINDO ENTREGADORES
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('34567890123', TO_DATE('1995-09-30', 'YYYY-MM-DD'), 'Carlos Lima'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('14408809444', TO_DATE('1986-05-15', 'YYYY-MM-DD'), 'Lucas Silva'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('09445676533', TO_DATE('2000-09-04', 'YYYY-MM-DD'), 'Gabriel Melo'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('32454367899', TO_DATE('1979-12-31', 'YYYY-MM-DD'), 'Miguel Carneiro'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('00912345678', TO_DATE('2001-04-15', 'YYYY-MM-DD'), 'Alvaro Neto'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('03212355678', TO_DATE('1998-05-30', 'YYYY-MM-DD'), 'Bianca Pessoa'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('12487954266', TO_DATE('1995-09-30', 'YYYY-MM-DD'), 'Eduarda Rocha'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('87632145677', TO_DATE('2000-03-27', 'YYYY-MM-DD'), 'Gabriela Ferraz'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('45676555523', TO_DATE('1999-01-14', 'YYYY-MM-DD'), 'Fabiana Gomes'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('24367899911', TO_DATE('2002-06-01', 'YYYY-MM-DD'), 'Julia Carvalho'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('55566677788', TO_DATE('1990-02-02', 'YYYY-MM-DD'), 'Rafael Pereira'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('66677788899', TO_DATE('1985-07-07', 'YYYY-MM-DD'), 'Marcela Souza'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('77788899900', TO_DATE('1988-08-08', 'YYYY-MM-DD'), 'Roberto Costa'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('88899900011', TO_DATE('1992-12-12', 'YYYY-MM-DD'), 'Sandra Dias'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('99900011122', TO_DATE('1975-03-03', 'YYYY-MM-DD'), 'Leonardo Pinto'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('00011122233', TO_DATE('1980-10-10', 'YYYY-MM-DD'), 'Mariana Freitas'));
+
+INSERT INTO tb_entregador (CPF, DataDeNascimento, Nome)
+VALUES (tp_entregador('11122233344', TO_DATE('1999-06-06', 'YYYY-MM-DD'), 'Diego Fernandes'));
+
+-- INSERINDO CONSUMIDORES
+
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('76543210987', TO_DATE('1995-09-30', 'YYYY-MM-DD'), 'Julia Almeida', '52000000', 'Rua 3', 789, 'Recife', 'Apto 3');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('12312312300', TO_DATE('1969-04-15', 'YYYY-MM-DD'), 'Zenaide Castro', '52061100', 'Rua ABC', 59, 'Recife', 'Apto 2702');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('86586586599', TO_DATE('2004-11-10', 'YYYY-MM-DD'), 'Juliana Valenca', '52061540', 'Rua 17 de Agosto', 2483, 'Recife', 'Apto 2301');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('43245676588', TO_DATE('1987-03-30', 'YYYY-MM-DD'), 'Clarissa Baltar', '32154679', 'Rua da Aurora', 50, 'Recife', 'Apto 301');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('32166677788', TO_DATE('1950-06-12', 'YYYY-MM-DD'), 'Gabriela Lucena', '12343456', 'Rua Real da Torre', 140, 'Recife', 'Apto 2');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('21345676555', TO_DATE('1986-07-02', 'YYYY-MM-DD'), 'Bruno Rezende', '52061540', 'Rua 17 de Agosto', 2720, 'Recife', 'Apto 1802');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('76540000987', TO_DATE('1957-01-03', 'YYYY-MM-DD'), 'Lucas Mendes', '51000000', 'Rua Virginia Loreto', 79, 'Recife', 'Apto 303');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('22543210987', TO_DATE('1994-11-03', 'YYYY-MM-DD'), 'Vinicius Albuquerque', '53400000', 'Rua das Gracas', 80, 'Recife', 'Apto 1401');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('98765432112', TO_DATE('1991-01-01', 'YYYY-MM-DD'), 'Renata Martins', '52012345', 'Avenida Brasil', 100, 'Recife', 'Casa 1');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('11223344556', TO_DATE('2003-03-03', 'YYYY-MM-DD'), 'André Costa', '52054321', 'Rua da Paz', 45, 'Recife', 'Apto 101');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('22334455667', TO_DATE('1982-02-02', 'YYYY-MM-DD'), 'Carla Mendes', '52011122', 'Avenida Central', 101, 'Recife', 'Casa 12');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('33445566778', TO_DATE('1978-04-04', 'YYYY-MM-DD'), 'Andressa Lima', '52022233', 'Rua das Flores', 202, 'Recife', 'Apto 303');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('44556677889', TO_DATE('1993-03-03', 'YYYY-MM-DD'), 'Felipe Cardoso', '52033344', 'Travessa da Luz', 303, 'Recife', 'Casa 2');
+INSERT INTO tb_consumidor (CPF, DataDeNascimento, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('55667788990', TO_DATE('2000-08-08', 'YYYY-MM-DD'), 'Marcelo Souza', '52044455', 'Alameda Santos', 404, 'Recife', 'Apto 405');
+
+-- INSERINDO FORNECEDOR
+
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('34567890000122', 'Padaria Pão Quente', '52000000', 'Rua C', 30, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('00056789000111', 'Frutaria Verd Campo', '52111000', 'Rua Silvana Salazar', 45, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('23456756700111', 'Padaria Casa Forte', '52061540', 'Rua 17 de Agosto', 1211, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('23999989000111', 'DOC', '51110000', 'Rua Irma Maria David', 200, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('23999555000111', 'Mix Matheus', '52340000', 'Rua Rui Barbosa', 144, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('23456789000111', 'Padaria Pão Quente', '52000000', 'Rua C', 30, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('23456000000111', 'Kojima', '41230000', 'Rua Antonio de Souza', 300, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('23456289000111', 'Tigres da Serra', '52000000', 'Rua Vicente', 60, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('32145678900111', 'Açougue Silva', '52110000', 'Rua Litorânea', 33, 'Olinda', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('76543210000111', 'Cozinha Gourmet', '52340000', 'Rua das Palmeiras', 77, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('12398765432111', 'Lava Jato Clean', '52022000', 'Rua da Paz', 90, 'Jaboatão', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('87965432198711', 'Armazém do Frango', '52066000', 'Avenida Recife', 100, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('54321567891011', 'Mercado São José', '52120000', 'Rua Floriano Peixoto', 40, 'Recife', '');
+INSERT INTO tb_fornecedor (CNPJ, Nome, CEP, Rua, Numero, Cidade, Complemento) VALUES ('65432109876511', 'Supermercado Barato', '52033000', 'Avenida dos Coqueiros', 150, 'Recife', '');
